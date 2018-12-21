35190693467371c10edb0c2dc86f511445ef9bd2
Adrian Sanchez <adri09ian@gmail.com>
2018-12-21 11:09:37 +0000

    SET KEY

--- GoogleReview/GoogleReviews/src/main/java/soc/ucm/es/GoogleReviews/App.java
+++ GoogleReview/GoogleReviews/src/main/java/soc/ucm/es/GoogleReviews/App.java
@@ -1,22 +1,31 @@
 package soc.ucm.es.GoogleReviews;
 
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ApiException, InterruptedException, IOException
     {
         System.out.println( "Hello World!" );
-        
         GeoApiContext context = new GeoApiContext.Builder()
-        	    .apiKey("AIza...")
-        	    .build();
+        	    .apiKey("AIzaSyBKfTFLkbKc0bwuT_kmDeEC9z7nfpSPbXg") // Directions API, Geocoding API
+				.build();
+				
         	GeocodingResult[] results =  GeocodingApi.geocode(context,
         	    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
         	Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        	System.out.println(gson.toJson(results[0].addressComponents));
+        	System.out.println(gson.toJson(results));
         	
     }
 }
