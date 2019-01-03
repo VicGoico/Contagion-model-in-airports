ef648ba5cd6e74c58bb760fb052335ba5bac2cf9
Adrian Sanchez <adsanc03@ucm.es>
2019-01-03 19:48:13 +0100

    Empieza el procesamiento del CSV

--- Modelo de contagio/src/modelo/metricas/carbonDioxide/CSVProcessor.java
+++ Modelo de contagio/src/modelo/metricas/carbonDioxide/CSVProcessor.java
@@ -4,17 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import modelo.makeUsesfullCSV;
 
 public class CSVProcessor {
 	String fileName;
 	private BufferedReader fileBuffer;
-	private makeUsesfullCSV healthMetric;
+	private makeUsesfullCSV processCSVDatas;
 	
 	public CSVProcessor(String fileName, makeUsesfullCSV mUfCSV) throws FileNotFoundException {
 		this.fileName = fileName;
-		this.healthMetric = mUfCSV;
+		this.processCSVDatas = mUfCSV;
 		
 		this.openFile();
 	}
@@ -23,21 +27,42 @@ public class CSVProcessor {
 		this.fileBuffer = new BufferedReader(new FileReader(this.fileName));
 	}
 	
-	private void readCSV() {
+	private Collection<String> splitLine(String line){
+		String[] lineParts = line.split(","); // Separo todas las lineas con ,
+		List<String> lnFParts = new ArrayList<String>();
+		String buff = "";
+		
+		
+		for (int i = 0; i < lineParts.length; i++) {
+			if(lineParts[i].startsWith("\"")) {
+				// He encontrado una cadena, uno todo eso de nuevo para procesarlo
+				buff = String.join(",", Arrays.copyOfRange(lineParts, i, lineParts.length));
+				System.out.println("CADENA DETECTADA: ");
+				System.out.println(buff);
+			} else {
+				lnFParts.add(lineParts[i]);
+			}
+		}
+		return null;
+	}
+	
+	private void readCSV() throws IOException {
 		String line;
 		
 		try {
 			while ((line = fileBuffer.readLine()) != null) {
+				Collection<String> ln = this.splitLine(line);
 				
+				// Una vez tengo separadas las lineas extraigo de cada una lo que me interesa...
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw e;
 		} finally {
 			if (this.fileBuffer != null) {
 				try {
 					this.fileBuffer.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					throw e;
 				}
 			}
 		}
