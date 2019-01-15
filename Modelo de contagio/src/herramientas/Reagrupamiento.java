ed05c52670f171c468a987bb4a8dabc7872ca116
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-01-15 13:20:37 +0100

    clase reagrupamiento para cambios de la tutoria

---
+++ Modelo de contagio/src/herramientas/Reagrupamiento.java
@@ -0,0 +1,107 @@
+package herramientas;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Reagrupamiento {
+
+	public static final String SEPARATORNODO = ",";
+	public static final String SEPARATORARISTA = ";";
+
+	public static final String QUOTE = "\"";
+	private static HashMap<Integer,Integer> equivalente;
+
+	public static void main(String[] args) {
+		equivalente = new HashMap<>();
+		nodos();
+		aristas();
+	}
+
+	private static void nodos() {
+		BufferedReader br = null;
+		
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("nodosCambiados2.csv"));
+			br = new BufferedReader(new FileReader("nodos.csv"));
+			String line = br.readLine();
+			String[] fieldsTitulo = line.split(SEPARATORNODO);
+			String titulo = "";
+			for(int j = 0; j < fieldsTitulo.length; j++) {
+				titulo = titulo + fieldsTitulo[j] + ",";
+			}
+			out.write(titulo + System.getProperty("line.separator"));
+			int i = 0;
+			line = br.readLine();
+			while (null != line) {
+				String[] fields = line.split(SEPARATORNODO);
+				equivalente.put(Integer.valueOf(fields[0]), i);
+				String aux = i + ",";
+				for(int j = 1; j < fields.length; j++) {
+					aux = aux + fields[j] + ",";
+				}
+				out.write(aux + System.getProperty("line.separator"));
+				line = br.readLine();			
+				i += 1;
+			}
+			out.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (null != br) {
+				try {
+					br.close();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	private static void aristas() {
+		BufferedReader br = null;
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("aristasCambiadas2.csv"));
+			br = new BufferedReader(new FileReader("aristas.csv"));
+			String line = br.readLine();
+			String[] fieldsTitulo = line.split(SEPARATORARISTA);
+			String titulo = "";
+			for(int j = 0; j < fieldsTitulo.length; j++) {
+				titulo = titulo + fieldsTitulo[j] + SEPARATORARISTA;
+			}
+			out.write(titulo + System.getProperty("line.separator"));
+			line = br.readLine();
+			while (null != line) {
+				String[] fields = line.split(SEPARATORARISTA);
+				if(!fields[3].equalsIgnoreCase("N") && !fields[5].equalsIgnoreCase("N")) {
+					String aux = "";
+					fields[3] = String.valueOf(equivalente.get(Integer.valueOf(fields[3])));
+					fields[5] = String.valueOf(equivalente.get(Integer.valueOf(fields[5])));
+					for(int j = 0; j < fields.length; j++) {
+						aux = aux + fields[j] + SEPARATORARISTA;
+					}
+					
+					out.write(aux + System.getProperty("line.separator"));
+				}			
+				line = br.readLine();	
+			}//3 y 5
+			out.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (null != br) {
+				try {
+					br.close();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
